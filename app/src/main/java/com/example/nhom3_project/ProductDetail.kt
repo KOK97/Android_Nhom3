00f1d9419f6305072a7335058f42bda411a59973
Vietlam303 <21211tt1008@mail.tdc.edu.vn>
2024-12-18 18:43:32 +0700

    fix

--- app/src/main/java/com/example/nhom3_project/ProductDetail.kt
+++ app/src/main/java/com/example/nhom3_project/ProductDetail.kt
@@ -1,20 +1,31 @@
 package com.example.nhom3_project
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ProductDetail : AppCompatActivity() {
+    private lateinit var btnExit :Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_product_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val productId = intent.getStringExtra("productClick")
+        Toast.makeText(this, "Clicked on: $productId", Toast.LENGTH_SHORT).show()
+    setControl()
+        setEvent()
+    }
+    private  fun setControl(){
+        btnExit = findViewById(R.id.btnExit)
+    }
+    private  fun setEvent(){
+        btnExit.setOnClickListener(){
+            finish()
         }
     }
 }
