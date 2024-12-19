b60a0f63c8ad2988cc9be47427f6a22c0d18f4f6
Vietlam303 <21211tt1008@mail.tdc.edu.vn>
2024-12-19 10:01:44 +0700

    detail

--- app/src/main/java/com/example/nhom3_project/Comment.kt
+++ app/src/main/java/com/example/nhom3_project/Comment.kt
@@ -11,10 +11,6 @@ class Comment : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_comment)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
