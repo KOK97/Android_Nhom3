4a9a302c38d5af3dc9a8d51063392c8ed6824568
Vietlam303 <21211tt1008@mail.tdc.edu.vn>
2024-12-19 22:58:57 +0700

    fix detai and get cmt

--- app/src/main/java/com/example/nhom3_project/ProductDetail.kt
+++ app/src/main/java/com/example/nhom3_project/ProductDetail.kt
@@ -1,21 +1,23 @@
 package com.example.nhom3_project
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.text.SpannableString
-import android.text.Spanned
-import android.text.style.ImageSpan
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.ListView
 import android.widget.RatingBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.database.DataSnapshot
@@ -33,17 +35,17 @@ class ProductDetail : AppCompatActivity() {
     private lateinit var ivProDetail: ImageView
     private lateinit var ratingBar : RatingBar
     private lateinit var tvTotalCMT : TextView
-    private lateinit var tvBoxCMT : TextView
+    private lateinit var lnBoxCMT : LinearLayout
+
 
     private lateinit var btnExit :Button
     private lateinit var btnAddCart :Button
     private lateinit var btnBuyNow :Button
     private lateinit var btnComment :Button
 
-
+    private lateinit var ivDefault: ImageView
     //nav
     private lateinit var navbarBott: BottomNavigationView
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -69,31 +71,37 @@ class ProductDetail : AppCompatActivity() {
         tvRating = findViewById(R.id.tvRating)
         ratingBar = findViewById(R.id.ratingBar)
 
+        ivDefault = findViewById(R.id.ivDefault)
         tvTotalCMT = findViewById(R.id.tvTotalCmt)
-        tvBoxCMT = findViewById(R.id.tvBoxCmt)
+        lnBoxCMT = findViewById(R.id.lvBoxCMT)
+
 
         //nav
         navbarBott = findViewById<BottomNavigationView>(R.id.bottom_navigationCart)
 
     }
     private  fun setEvent(){
-        val getProductID = intent.getStringExtra("productClick")
-        Toast.makeText(this, "Clicked on: $getProductID", Toast.LENGTH_SHORT).show()
-        val databaseReference = FirebaseDatabase.getInstance().getReference("products")
-        val productId = getProductID
 
+        //lay id duoc gui
+        val getProductID = intent.getStringExtra("productClick")
+//        Toast.makeText(this, "Clicked on: $getProductID", Toast.LENGTH_SHORT).show()
+        val productId = getProductID
+        //connet table product
+        val databaseReference = FirebaseDatabase.getInstance().getReference("products")
+        //connet table comments
+        val databaseReferenceCMT = FirebaseDatabase.getInstance().getReference("Comments")
         if (productId != null) {
-            databaseReference.child(productId).addListenerForSingleValueEvent(object :
-                ValueEventListener {
+            //xu ly product detai
+            //xu ly voi duy 1 child co id la proID
+            databaseReference.child(productId).addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
                     if (snapshot.exists()) {
-                        // Retrieve product details from the snapshot
+                        //get data
                         val productName = snapshot.child("name").getValue(String::class.java) ?: "No Name"
                         val productDesc = snapshot.child("desc").getValue(String::class.java) ?: "No Desc"
                         val productType = snapshot.child("type").getValue(String::class.java) ?: "No Type"
                         val productPrice = snapshot.child("price").getValue(Int::class.java) ?: 0
                         val imageUrl2 = snapshot.child("imageUrl").getValue(String::class.java) ?: "No image"
-                        //set data sp2
 
                         //lay anh tu drawble
                         val imageResId2 = resources.getIdentifier(imageUrl2, "drawable", packageName)
@@ -111,33 +119,75 @@ class ProductDetail : AppCompatActivity() {
                                 .override(400, 150)
                                 .into(ivProDetail)
                         }
+                        //set data sp2
                         tvNameDetail.text = productName
                         tvPriceDetail.text = productPrice.toString()
-                        tvIdDetait.text = productId
-                        tvType.text = productType
+                        tvIdDetait.text = "Mã: $productId"
+                        tvType.text = "Giới tính: $productType"
                         tvDesc.text = productDesc
-                        tvTotalCMT.text = "Tổng cộng 0 đánh giá từ khách hàng"
-
-                        val text = SpannableString("\u200B")
-
-                        // Tải hình ảnh
-                        val drawable = ContextCompat.getDrawable(this@ProductDetail, R.drawable.nocmt)
-                        drawable?.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
-
-                        // Chèn hình ảnh vào văn bản
-                        val imageSpan = ImageSpan(drawable!!)
-                        text.setSpan(imageSpan, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-
-                        tvBoxCMT.text = text
+                        btnComment.setOnClickListener()
+                        {
+                            if(productId != null){
+                                val arr = arrayOf("$productId","$productName","$imageUrl2")
+                                val intent = Intent(this@ProductDetail, Comment::class.java)
+                                intent.putExtra("productCMT", arr)
+                                startActivity(intent)
+                            }
+                        }
                     } else {
-                        // Handle case when the product doesn't exist
                         Log.e("Firebase", "Product not found")
                     }
                 }
                 override fun onCancelled(error: DatabaseError) {
                     Log.e("FirebaseError", "Failed to read data", error.toException())
                 }
+            })
+            //ket thuc xu ly prodetail
 
+            //xu ly comment
+            databaseReferenceCMT.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    var haveCmt = false
+                    var cmtnum = 0
+                    // Lặp qua tất cả các bình luận
+                    for (snapCMT in snapshot.children) {
+                        //get id san pham dc danh gia
+                        val idProCmt = snapCMT.child("idProduct").getValue(String::class.java)?: "No id"
+                        // show if id dc nhan == idProcmt
+                        if (idProCmt == productId) {
+                            cmtnum ++ // dem so luong cmt
+
+                            val nameUsercmt = snapCMT.child("nameUser").getValue(String::class.java) ?: "No Name"
+                            val cmt = snapCMT.child("cmt").getValue(String::class.java)?: "No Comment"
+                            // tao view tu layout item
+                            val inflater = LayoutInflater.from(this@ProductDetail)
+                            val view = inflater.inflate(R.layout.layout_post_comment, lnBoxCMT, false)
+
+                            view.findViewById<TextView>(R.id.tvNameUserCMT).text = nameUsercmt
+                            view.findViewById<TextView>(R.id.tvCmt).text = cmt
+
+                            if (cmt != null){
+                                haveCmt = true
+                            }
+                           lnBoxCMT.addView(view)
+
+                        }
+                        tvTotalCMT.text = "Tổng cộng $cmtnum đánh giá từ khách hàng"
+
+                        if(haveCmt == true){ //neu sp co cmt -> show cmt
+                            lnBoxCMT.visibility = View.VISIBLE
+                            ivDefault.visibility = View.GONE
+                        }
+                        else{ // show image default
+                            lnBoxCMT.visibility = View.GONE
+                            ivDefault.visibility = View.VISIBLE
+                        }
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.e("FirebaseError", "Failed to read data", error.toException())
+                }
             })
         }
 
@@ -145,14 +195,7 @@ class ProductDetail : AppCompatActivity() {
         btnExit.setOnClickListener(){
             finish()
         }
-        btnComment.setOnClickListener()
-        {
-            if(productId != null){
-            val intent = Intent(this@ProductDetail, Comment::class.java)
-            intent.putExtra("productId", productId)
-            startActivity(intent)
-            }
-        }
+
         btnAddCart.setOnClickListener(){
             if(productId != null){
                 val intent = Intent(this@ProductDetail, CartActivity::class.java)
